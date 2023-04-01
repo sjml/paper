9cff0f7d2970fc226187d9c3a1badfa2c1780bb6
Shane Liesegang <shane@techie.net>
2023-03-31 23:06:14 -0400

    trimmed wordcount should ignoreh headers, too

--- resources/scripts/stripped_md.lua
+++ resources/scripts/stripped_md.lua
@@ -1,4 +1,4 @@
--- strips out Meta, Note, and Cite elements to just give main text of Markdown,
+-- strips out Meta, Note, Cite, and Header elements to just give main text of Markdown,
 --    for use in calculating word count of file
 
 function Meta(m)
@@ -10,5 +10,9 @@ function Cite(c)
 end
 
 function Note(n)
+  return {}
+end
+
+function Header(h)
   return {}
 end
